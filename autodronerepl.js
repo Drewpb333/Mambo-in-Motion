6bfe4938c9c60c922ed7828dc1aaad4f20bed1e5
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-05-17 19:52:58 -0400

    pseudocode autodrone functionality

--- autodronerepl.js
+++ autodronerepl.js
@@ -1,3 +1,24 @@
 //have packages required here
 
-//
+//best way to execute this would be to receive an array of objects from UI
+//each object would have an action, a distance (length), and a time (ms) property
+//we would loop through each object in the array
+//each action would be matched with a method and executed
+//each method would also have to account for distance or time
+
+const autodrone = require("ardrone-autonomy");
+var mission = autodrone.createMission();
+
+
+
+//runs drone
+mission.run(function (err, result) {
+    if (err) {
+        console.trace("Oops, something bad happened: %s", err.message);
+        mission.client().stop();
+        mission.client().land();
+    } else {
+        console.log("Mission success!");
+        process.exit(0);
+    }
+});
