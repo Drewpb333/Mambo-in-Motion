8c26917424fe36a6edc563852bc7d38b4f523bbf
Sirish Kanukunta <sirishkanukunta@Sirishs-MacBook-Pro.local>
2018-05-18 10:36:03 -0400

    changes to userAuth

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+import ReactDOM from 'react-dom';
+import './index.css';
+//yaimport 'bootstrap/dist/css/bootstrap.css';
+import { makeMainRoutes } from './routes';
+
+const routes = makeMainRoutes();
+
+ReactDOM.render(
+  routes,
+  document.getElementById('root')
+);
