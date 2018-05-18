d5b53c7314e380d5e30bd41b3536c9dc42ee8e9f
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-05-17 20:06:02 -0400

    flight methods object added

--- autodronerepl.js
+++ autodronerepl.js
@@ -9,7 +9,22 @@
 const autodrone = require("ardrone-autonomy");
 var mission = autodrone.createMission();
 
+let flightPlan = [];
 
+flightPlan.forEach(action => )
+
+
+let flightMethods = {
+    "takeoff": () => mission.takeoff(),
+    "zero": () => mission.zero(),
+    "altitude": height => mission.altitude(height),
+    "forward": distance => mission.forward(distance),
+    "right": distance => mission.right(distance),
+    "backward": distance => mission.backward(distance),
+    "left": distance => mission.left(distance),
+    "hover": ms => mission.hover(ms),
+    "land": () => mission.land()
+}
 
 //runs drone
 mission.run(function (err, result) {
