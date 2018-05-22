a92528dd79efb4f153c2ea1f2b1fa47f5188d21f
Asif Kabir <Asif6152000@Asifs-MacBook-Air.local>
2018-05-22 10:39:52 -0400

    saving previous changes

---
+++ models/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS userAuth_db;
+
+CREATE DATABASE userAuth_db;
+
+USE userAuth_db;
+
+CREATE TABLE users (
+	id INTEGER (11) AUTO_INCREMENT NOT NULL,
+	
+	username VARCHAR(30) NOT NULL,
+	
+	password VARCHAR(30) NOT NULL,
+	
+	PRIMARY KEY (id)
+	
+);
