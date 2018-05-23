355f6c341665b03932cce842ac9610482e08e27a
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-05-23 12:01:26 -0400

    modifies server.js and package.json for testing purposes

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
-// const express = require("express");
-// const path = require("path");
-// const bodyParser = require("body-parser");
-// const PORT = process.env.PORT || 3001;
-// const app = express();
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-// // Define middleware here
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// // Serve up static assets (usually on heroku)
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("client/build"));
 // }
 
-// // Define API routes here
+app.get("/css/styles.css", ( req, res ) => {
+  res.sendFile( __dirname + "/css/styles.css" );
+});
+app.get("*", ( req, res ) => {
+  res.sendFile( __dirname + "/index.html" );
+})
 
-// // Send every other request to the React app
-// // Define any API routes before this runs
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
 
-// app.listen(PORT, () => {
-//   console.log(`🌎 ==> Server now on port ${PORT}!`);
-// });
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
